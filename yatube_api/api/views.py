92986432f600de35f1a5758c2fd608ce2bc69b0f
Андреев Петр <andreev.pete@gmail.com>
2022-09-07 16:19:45 +0300

    aa

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post, User
-from rest_framework import mixins, filters, viewsets
+from posts.models import Group, Post
+from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import IsAuthOnly, IsAuthorOrReadOnly
